6d2567853d911179421071dc2a4b5b03c83751ac
palakkhare597 <palakkhare597@gmail.com>
2026-07-08 15:13:44 +0530

    Add files via upload

---
+++ app (3).py
@@ -0,0 +1,62 @@
+import streamlit as st
+import joblib
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Page configuration
+st.set_page_config(page_title="Credit Scoring Model", page_icon="💳")
+
+st.title("💳 Credit Scoring Model")
+st.write("Decision Tree and Random Forest Performance")
+
+# Load trained Random Forest model
+model = joblib.load("random_forest.pkl")
+
+# Load feature names
+feature_names = joblib.load("feature_names.pkl")
+
+st.header("Model Information")
+
+st.write("**Algorithm:** Random Forest Classifier")
+
+st.write("**Task:** Credit Scoring Prediction")
+
+st.success("Model trained successfully.")
+
+# Feature Importance
+st.header("Feature Importance")
+
+importance = pd.DataFrame({
+    "Feature": feature_names,
+    "Importance": model.feature_importances_
+})
+
+importance = importance.sort_values(by="Importance", ascending=False)
+
+st.dataframe(importance)
+
+fig, ax = plt.subplots(figsize=(8,5))
+ax.bar(importance["Feature"], importance["Importance"])
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+st.pyplot(fig)
+
+st.markdown("---")
+st.write("Developed using Python, Scikit-learn and Streamlit.")
+#Evaluation metrics
+metrics = joblib.load("metrics.pkl")
+
+st.header("Model Performance")
+
+st.write("### Accuracy")
+st.write(metrics["Accuracy"])
+
+st.write("### ROC-AUC Score")
+st.write(metrics["ROC_AUC"])
+
+st.write("### Classification Report")
+st.text(metrics["Classification_Report"])
+
+st.write("### Confusion Matrix")
+st.write(metrics["Confusion_Matrix"])
